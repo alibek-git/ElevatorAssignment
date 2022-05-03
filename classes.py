edc4746bed34c179ee14466940a0f158c7f80d67
Alibek <alibek1991@gmail.com>
2022-05-03 18:12:12 +0600

    Initial commit

---
+++ classes.py
@@ -0,0 +1,41 @@
+class Building:
+
+    def __init__(self, storeys):
+        self.storeys = storeys
+        self.floors_list = []
+
+    def __str__(self):
+        return f'The building is {self.storeys} floors tall'
+
+    class Floor:  # Floor will be an inner class of the
+
+        def __init__(self, floor_id):
+            self.floor_id = floor_id + 1
+            self.passengers_list = []
+
+        def __repr__(self):
+            return f'Floor #{self.floor_id} has {len(self.passengers_list)} passengers'
+
+
+class Elevator:
+
+    passengers_in_the_elevator = []
+
+    def __init__(self, max_floor, current_floor=1, max_capacity=5):
+        self.current_floor = current_floor
+        self.max_capacity = max_capacity
+        self.max_floor = max_floor
+
+
+class Passenger:
+
+    def __init__(self, passenger_id, current_floor, desired_floor, is_on_desired_floor=False):
+        self.passenger_id = passenger_id + 1
+        self.current_floor = current_floor + 1
+        self.desired_floor = desired_floor
+        self.is_on_desired_floor = is_on_desired_floor
+        self.direction = 'Up' if self.current_floor < self.desired_floor else 'Down'
+
+    def __repr__(self):
+        return f'Passenger {self.passenger_id} is currently on floor {self.current_floor} and is going to floor ' \
+               f'{self.desired_floor}. Direction: {self.direction} '
---
+++ main.py
@@ -0,0 +1,23 @@
+from classes import *
+import random
+from time import sleep
+
+power_on = True
+storeys = random.randint(5, 10)  # Randomizing the number of floors our building object will have
+
+#  Creating instances of Building and Passengers
+new_building = Building(storeys)  # Initialize Building instance
+print(new_building)
+sleep(1)
+for floor in range(storeys):
+    new_floor = Building.Floor(floor)
+    new_building.floors_list.append(new_floor)
+    for _ in range(random.randint(0, 5)):
+        new_passenger = Passenger(_, floor, random.randint(1, storeys))
+        new_building.floors_list[floor].passengers_list.append(new_passenger)
+    print(new_floor)
+    sleep(1)
+    for y in new_floor.passengers_list:
+        sleep(1)
+        print(y)
+
